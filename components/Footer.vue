57916006db90ad0a4a3bc1a9d207186e053df433
神谷友樹 <jetree900@gmail.com>
2020-01-26 00:46:33 +0900

    header footer component 作成

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <h2>footer</h2>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  width: 100%;
+  background-color: #333;
+  position: fixed;
+  bottom: 0;
+}
+.container {
+  margin: 0 auto;
+  max-width: 800px;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="container">
+      <h1>つつじバスいまどこ</h1>
+    </div>
+  </header>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+  background-color: #333;
+}
+.container {
+  margin: 0 auto;
+  max-width: 800px;
+}
+</style>
