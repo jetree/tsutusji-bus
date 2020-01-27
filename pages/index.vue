df2510a6ac7457b8f979c8e99c06114e4970e6dd
神谷友樹 <jetree900@gmail.com>
2020-01-27 23:47:56 +0900

    路線データを表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
     <div>
-      <button @click.prevent="get_data">
-        取得
-      </button>
-      {{ data }}
-
       <ul>
-        <li v-for="item in data">{{ item.id }}:{{ item.name }}</li>
+        <li v-for="rosen in rosen" :key="rosen.id">
+          <button @click.prevent="get_data(rosen.id)">
+            {{ rosen.name }}
+          </button>
+        </li>
+      </ul>
+      {{ data }}
+      <ul>
+        <li v-for="item in data.rosen">{{ item.id }}:{{ item.name }}</li>
       </ul>
     </div>
   </div>
@@ -22,20 +25,111 @@ export default {
   components: {},
   data: () => {
     return {
-      data: {},
-      list: []
+      rosen: [
+        {
+          id: '1',
+          name: '循環線'
+        },
+        {
+          id: '2',
+          name: '幹線'
+        },
+        {
+          id: '3',
+          name: '鯖江南・新横江線'
+        },
+        {
+          id: '4',
+          name: '神明線'
+        },
+        {
+          id: '5',
+          name: '片上・北中山線'
+        },
+        {
+          id: '6',
+          name: '立待線'
+        },
+        {
+          id: '7',
+          name: '吉川線'
+        },
+        {
+          id: '8',
+          name: '豊線'
+        },
+        {
+          id: '9',
+          name: '中河・北中山線'
+        },
+        {
+          id: '10',
+          name: '河和田線'
+        },
+        {
+          id: '11',
+          name: '通学便 市内高校ルート'
+        },
+        {
+          id: '12',
+          name: '通学便 片上・北中山ルート'
+        },
+        {
+          id: '13',
+          name: '通学便 立待ルート'
+        },
+        {
+          id: '14',
+          name: '通学便 吉川ルート'
+        },
+        {
+          id: '15',
+          name: '通学便 豊ルート'
+        },
+        {
+          id: '16',
+          name: '通学便 中河・北中山ルート'
+        },
+        {
+          id: '17',
+          name: '通学便 河和田ルート'
+        },
+        {
+          id: '99',
+          name: '福鉄鯖浦線'
+        }
+      ],
+      data: ''
     }
   },
+  // async mounted() {
+  //   const json = await this.$jsonp('/api/busstopLookup.php', {
+  //     rosenid: 1,
+  //     callbackName: 'get'
+  //   })
+  //   console.log(json)
+  //   this.data = json
+  // },
   methods: {
-    async get_data() {
-      const response = await this.$axios.get('/api/busstopLookup.php', {
-        params: {
-          rosenid: 1
-        }
+    async get_data(id) {
+      const json = await this.$jsonp('/api/rosenidLookup.php', {
+        callbackName: 'get'
       })
-      this.data = response.data
-      console.log(JSON.stringify(this.data))
-      this.list = response.data
+      console.log(id)
+      this.data = json
+      //   this.$jsonp('/api/busstopLookup.php', {
+      //     params: {
+      //       rosenid: 2
+      //     }
+      //   })
+      //     .then((json) => {
+      //       console.log(json)
+      //       this.data = json
+      //     })
+      //     .catch((err) => {
+      //       console.log(err)
+      //       // エラーの時
+      //     })
     }
   }
 }
