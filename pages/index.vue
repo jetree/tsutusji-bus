63b582177ed63fca6623fc22d0b607a9ba6666b0
神谷友樹 <jetree900@gmail.com>
2020-01-27 21:48:46 +0900

    axiosでデータ取得をトライ

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,46 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        bus
-      </h1>
-      <h2 class="subtitle">
-        My magnificent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <button @click.prevent="get_data">
+        取得
+      </button>
+      {{ data }}
+
+      <ul>
+        <li v-for="item in data">{{ item.id }}:{{ item.name }}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  components: {},
+  data: () => {
+    return {
+      data: {},
+      list: []
+    }
+  },
+  methods: {
+    async get_data() {
+      const response = await this.$axios.get('/api/busstopLookup.php', {
+        params: {
+          rosenid: 1
+        }
+      })
+      this.data = response.data
+      console.log(JSON.stringify(this.data))
+      this.list = response.data
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  max-width: 800px;
+  background-color: #ddd;
 }
 </style>
