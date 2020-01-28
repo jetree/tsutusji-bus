6b00e1cba7a227d7d8405915cb90b39663d228e7
神谷友樹 <jetree900@gmail.com>
2020-01-28 21:45:07 +0900

    SelectRosen Button Component作成

---
+++ components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click.prevent="get">
+    {{ name }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  height: 80px;
+  min-width: 100px;
+  background-color: aqua;
+}
+</style>
---
+++ components/SelectRosen.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="menu-area">
+      <Button v-for="rosen in rosenList" :name="rosen.name" />
+      <!-- <div v-for="rosen in rosenList">{{ rosen.name }}</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '~/components/Button.vue'
+
+export default {
+  components: {
+    Button
+  },
+  props: {
+    rosenList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu-area {
+  display: flex;
+  overflow-x: scroll;
+}
+</style>
