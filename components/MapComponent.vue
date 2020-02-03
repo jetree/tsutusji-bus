ee7a7566bd1d25bc398fd1e292ffb42d7c59b865
神谷友樹 <jetree900@gmail.com>
2020-02-03 23:06:17 +0900

    地図上にバス停をプロット

--- components/MapComponent.vue
+++ components/MapComponent.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <button @click.prevent="set_busstop_icon()">
+      click
+    </button>
     <div id="map" style="max-width:800px; height:70vh" />
   </div>
 </template>
@@ -14,14 +17,36 @@ export default {
       type: Number
     }
   },
+  data: () => {
+    return {
+      ymap: ''
+    }
+  },
+  computed: {
+    busstop () {
+      return this.$store.getters['bus/busstop']
+    }
+  },
   mounted () {
     this.createMap()
   },
   methods: {
     createMap () {
-      console.log('ok')
-      const ymap = new Y.Map('map')
-      ymap.drawMap(new Y.LatLng(this.longitude, this.latitude), 13, Y.LayerSetId.NORMAL)
+      this.ymap = new Y.Map('map')
+      this.ymap.drawMap(new Y.LatLng(this.latitude, this.longitude), 13, Y.LayerSetId.NORMAL)
+    },
+    set_busstop_icon () {
+      this.ymap.clearFeatures()
+      if (this.busstop == null) {
+        console.log('syuuryou')
+        return
+      }
+      const markers = []
+      markers.push(new Y.Marker(new Y.LatLng(this.latitude, this.longitude)))
+      for (let i = 0; i < this.busstop.length; i++) {
+        markers.push(new Y.Marker(new Y.LatLng(this.busstop[i].latitude, this.busstop[i].longitude)))
+      }
+      this.ymap.addFeatures(markers)
     }
   }
 }
