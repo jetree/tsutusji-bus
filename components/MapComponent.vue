167b0b72dd0405b0e8698cd500e28cc93aa62abf
神谷友樹 <jetree900@gmail.com>
2020-02-04 22:50:08 +0900

    路線に合わせた縮尺、範囲が表示されるよう変更

--- components/MapComponent.vue
+++ components/MapComponent.vue
@@ -29,7 +29,6 @@ export default {
   },
   watch: {
     busstop (val, old) {
-      // console.log('watch', val)
       this.set_busstop_icon()
     }
   },
@@ -39,20 +38,34 @@ export default {
   methods: {
     createMap () {
       this.ymap = new Y.Map('map')
-      this.ymap.drawMap(new Y.LatLng(this.latitude, this.longitude), 13, Y.LayerSetId.NORMAL)
+      this.ymap.drawMap(new Y.LatLng(this.latitude, this.longitude), 14, Y.LayerSetId.NORMAL)
     },
     set_busstop_icon () {
       this.ymap.clearFeatures()
       if (this.busstop == null) {
-        console.log('syuuryou')
         return
       }
       const markers = []
-      markers.push(new Y.Marker(new Y.LatLng(this.latitude, this.longitude)))
+      const latitudeList = []
+      const longitudeList = []
+      const icon = new Y.Icon('http://tutujibus.com/image/busstop32.png')
       for (let i = 0; i < this.busstop.length; i++) {
-        markers.push(new Y.Marker(new Y.LatLng(this.busstop[i].latitude, this.busstop[i].longitude)))
+        markers.push(new Y.Marker(new Y.LatLng(this.busstop[i].latitude, this.busstop[i].longitude), { icon }))
+        latitudeList.push(this.busstop[i].latitude)
+        longitudeList.push(this.busstop[i].longitude)
       }
       this.ymap.addFeatures(markers)
+
+      // 地図の表示範囲を変更
+      // 最も離れた２地点を作る
+      const pointALatitude = Math.max.apply(null, latitudeList)
+      const pointALongitude = Math.max.apply(null, longitudeList)
+      const pointBLatitude = Math.min.apply(null, latitudeList)
+      const pointBLongitude = Math.min.apply(null, longitudeList)
+      this.ymap.drawBounds(
+        new Y.LatLngBounds(
+          new Y.LatLng(pointBLatitude, pointBLongitude),
+          new Y.LatLng(pointALatitude, pointALongitude)), Y.LayerSetId.NORMAL)
     }
   }
 }
